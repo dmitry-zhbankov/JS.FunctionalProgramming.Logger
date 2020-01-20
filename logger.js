73220bb8492fb8d7acd148e45ed824ecb993ca29
Dmitry Zhbankov <dmitry.zhbankov@gmail.com>
2020-01-20 14:21:24 +0300

    Add class inheritance implementation

--- logger.js
+++ logger.js
@@ -1,5 +1,7 @@
+//Functional pattern
+
 function log(message) {
-console.log(message);
+    console.log(message);
 }
 
 function log2(...msgArgs){
@@ -13,6 +15,39 @@ function log3(...msgArgs){
     log2(dateStr, ...msgArgs);
 }
 
-log("Hello World");
+log("Hello World !");
 log2("Hello", "World", "!");
 log3("Hello", "World", "!");
+
+
+//Class inheritance pattern
+
+class BaseLogger {
+    log(message) {
+        console.log(message);
+    };
+}
+
+class TableLogger extends BaseLogger{
+    log(...msgArgs) {
+        let message=msgArgs.join(" | ");
+        super.log(message);
+    };
+}
+
+class TableTimeLogger extends TableLogger{
+    log (...msgArgs) {
+        let date=new Date();
+        let dateStr=`${date.toLocaleDateString()}, ${date.toLocaleTimeString()}`;
+        super.log(dateStr, ...msgArgs);
+    }
+}
+
+let baseLogger=new BaseLogger();
+baseLogger.log("Hello World !");
+
+let tableLogger=new TableLogger();
+tableLogger.log("Hello", "World", "!");
+
+let tableTimeLogger=new TableTimeLogger();
+tableTimeLogger.log("Hello", "World", "!");
